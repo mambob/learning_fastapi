85b9f5fcfd670952216c78d056defb994e37a786
Jorge Cabrera <jorge.cgc0@gmail.com>
2026-03-15 00:57:21 +0000

    testing response_class

--- src/learning_fastapi/main.py
+++ src/learning_fastapi/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "¡Hola, FastAPI funciona!"}
+posts: list[dict] = [
+    {"id": 1, "title": "Primer Post", "content": "Este es el primer post"},
+    {"id": 2, "title": "Segundo Post", "content": "Este es el segundo post"},
+]
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return "<h1>Bienvenido a mi API</h1>"
+
+@app.get("/posts")
+def get_posts():
+    return posts
 
 @app.get("/saludo/{nombre}")
 def saludo(nombre: str):
